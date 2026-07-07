cf0d7be8095db4a7d8fd63210bbd93fd132c6a1b
Harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2026-07-07 14:36:48 +0530

    Create code.java

---
+++ CSES-sheet/Graph/Longest-Flight-Route-task-1680/code.java
@@ -0,0 +1,143 @@
+import java.util.*;
+
+public class Demo {
+
+    // static class Edge {
+    // int u, v, w;
+
+    // Edge(int u, int v, int w) {
+    // this.u = u;
+    // this.v = v;
+    // this.w = w;
+    // }
+    // }
+
+    static List<List<Integer>> adj;
+
+    public static boolean dfs(int node, boolean[] vis, boolean[] rec,Stack<Integer> topo) {
+
+        rec[node] = true;
+        vis[node] = true;
+     
+        List<Integer> destinations = adj.get(node);
+        for (int child : destinations) {
+
+            if (rec[child]) {
+               return true;
+            } else {
+                if (!vis[child]) {
+                    if (dfs(child, vis, rec,topo)) {
+                        return true;
+                    }
+
+                }
+            }
+        }
+           
+        rec[node] = false;
+        topo.add(node);
+        return false;
+    }
+
+    public static void solve(Scanner sc) {
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        adj = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj.get(u).add(v);
+        }
+
+        boolean[] vis = new boolean[n + 1];
+        boolean[] rec = new boolean[n + 1];
+        boolean isCycle = false;
+
+        Stack<Integer> topo=new Stack<>();
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i]) {
+                boolean status = dfs(i, vis, rec,topo);
+
+                if (status) {
+                    isCycle = true;
+                    break;
+                }
+            }
+        }
+
+        if (isCycle) {
+            System.out.println("IMPOSSIBLE");
+            return;
+        }
+
+     List<Integer> topoList =new ArrayList<>();
+
+     while(!topo.isEmpty()){
+        topoList.add(topo.pop());
+     }
+
+    long[] dp =new long[n+1];
+    long INF=-(long)1e18;
+    int[] parent=new int[n+1];
+
+    Arrays.fill(dp,INF);
+
+    dp[1]=1;
+    
+    for(int u:topoList){
+        
+        if(dp[u]==INF)continue;
+
+        List<Integer> edges=adj.get(u);
+        for(int v:edges){
+
+            if(dp[u]+1>dp[v]){
+                dp[v]=dp[u]+1;
+                parent[v]=u;
+            }
+        }
+    }
+
+    if(dp[n]==INF){
+      System.out.println("IMPOSSIBLE");
+      return;
+    }
+
+    List<Integer> path =new ArrayList<>();
+
+    int cur=n;
+
+    while(cur!=0){
+        path.add(cur);
+        cur=parent[cur];
+    }
+
+    Collections.reverse(path);
+
+    System.out.println(dp[n]);
+    for(int i:path){
+        System.out.print(i+" ");
+    }
+
+
+
+    
+     
+
+        
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        solve(sc);
+        sc.close();
+    }
+}
